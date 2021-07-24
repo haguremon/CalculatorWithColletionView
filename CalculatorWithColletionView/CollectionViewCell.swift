a3c6f23597a5b643fda3da70416dd17ccc4262fa
Yuta iwasaki <yanyong0127@gmail.com>
2021-07-24 21:52:30 +0900

    UI調整中

---
+++ CalculatorWithColletionView/CollectionViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  CollectionViewCell.swift
+//  CalculatorWithColletionView
+//
+//  Created by IwasakIYuta on 2021/07/24.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    var numberLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = .boldSystemFont(ofSize: 33)
+        label.backgroundColor = .systemGray
+        label.clipsToBounds = true //cellからはみ出さないようにする
+        return label
+    }()
+    override init(frame: CGRect) { //初期化した時に呼ばれる所
+        super.init(frame: frame)
+        addSubview(numberLabel)//addSubview(UIView)でVIewにラベルを表示することができる
+        numberLabel.frame.size = self.frame.size //numberLabelのサイズをセルのフレームと同じにする
+        numberLabel.layer.cornerRadius = self.frame.height / 2
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- CalculatorWithColletionView/ViewController.swift
+++ CalculatorWithColletionView/ViewController.swift
@@ -9,11 +9,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    //cellに配置するするやつ
+    let calculatorScreen = [
+        ["AC","+/-","%","÷"],
+        ["7","8","9","×"],
+        ["4","5","6","-"],
+        ["1","2","3","+"],
+        ["0",".","="]
+    
+    ]
+    @IBOutlet weak var collectionViewHeightConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.backgroundColor = .clear
+        view.backgroundColor = .systemGreen
+        numberLabel.font = .boldSystemFont(ofSize: 70)
     }
 
+
+}
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout { //UICollectionViewDelegateFlowLayoutでレイアウトを作成することができる
+    //セッションの間のHeaderの大きさとかを決める
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        
+        return .init(width: collectionView.frame.width, height: 10)
+    }
+    //sizeForItemAtでcollectionViewのせるの大きさを決める
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width / 5
+        return .init(width: width, height: width)
+    }
+    
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return calculatorScreen.count //5
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return calculatorScreen[section].count//section毎の数
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        //まずセクションを選んでそれのロウを指定しする
+        cell.numberLabel.text = calculatorScreen[indexPath.section][indexPath.row]
+        // cell.backgroundColor = .link
+        return cell
+    }
+    
+    
+    
 
 }
 
